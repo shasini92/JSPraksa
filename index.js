30b3bf2b33c6fef44be801d85599168f912571aa
Sinisa Milicic <sinisamilicic@Sinisas-MacBook-Air.local>
2019-10-02 10:12:29 +0200

    JS Anonymous functions.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-for (var i = 0; i <= 10; i++) {
-  console.log(i);
+// Using map to create a new array of squared numbers
+let arrayOfNumbers = [1, 2, 3, 4, 5];
+
+let squaredNumbers = arrayOfNumbers.map(function(number) {
+  return number * number;
+});
+console.log(squaredNumbers);
+
+// IIFE
+let a = 5;
+let b = 7;
+let result = (function() {
+  console.log(a + b);
+})();
+
+// Creating an arrow function
+function timesTwo(number) {
+  return number * 2;
 }
-console.log("The value of i:" + i);
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
-}
-console.log("The value of i:" + i);
+let ArrowTimesTwo = number => number * 2;
 
-let i = 20;
-const outsideFunction = function() {
-  console.log(i);
-};
-
-const insideFunction = function() {
-  let i = 10;
-};
-console.log("The value of i:" + i);
+console.log(ArrowTimesTwo(5));
