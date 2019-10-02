19e7854f02d2c1985c17a989c362baaf1530fe07
Sinisa Milicic <sinisamilicic@Sinisas-MacBook-Air.local>
2019-10-02 11:58:02 +0200

    JS/JSON Objects.

--- index.js
+++ index.js
@@ -1,76 +1,29 @@
-// Creating colors array anod using for and forEach
+let person = {
+  firstName: "Sinisa",
+  lastName: "Milicic",
+  age: 27
+};
 
-let colors = ["Red", "Green", "Blue", "Yellow"];
+let personJSON = {
+  "firstName" : "Sinisa",
+  "lastName" : "Milicic",
+  "age": 27
+};
 
-for (let i = 0; i < colors.length; i++) {
-  console.log(`Color in the for loop is: ${colors[i]}`);
-}
+// Because personJSON is already an object it cannot be parsed again to an object, so I stringified it in order to be able to parse it correctly, otherwise we get the "Uncaught SyntaxError: Unexpected token o in JSON" error
 
-colors.forEach(color => console.log(`Color in the forEach loop is: ${color}`));
+let parsedPerson = JSON.parse(JSON.stringify(personJSON));
+console.log(parsedPerson);
 
-// Array rotation
-let array = [1, 2, 3, 4, 5];
+let stringifiedPerson = JSON.stringify(person);
+console.log(stringifiedPerson);
 
-let arrayRotate = (array, count) => {
-  for (let i = 0; i < count; i++) {
-    array.unshift(array.pop());
+// JS and JSON array of numbers
+
+let numbersJS = [1,2,3,4,5];
+let numbersJSON = {"numbers":[1,2,3,4,5]};
+
+let parsedNumbers = JSON.parse(JSON.stringify(numbersJSON));
+for (i in parsedNumbers.numbers) {
+    console.log(parsedNumbers.numbers[i]);
   }
-
-  console.log(
-    `The new array is [${array}] and it has been shifted by ${count} steps.`
-  );
-};
-
-arrayRotate(array, 2);
-
-// Sum of numbers using reduce
-let numbers = [1, 2, 3, 4, 5];
-
-let total = array =>
-  array.reduce((accumulator, currentValue) => accumulator + currentValue, 50);
-
-console.log(`The sum is ${total(numbers)}.`);
-
-// A function that creates an incremental and decremental array.
-
-let createArray = numberOfElements => {
-  let array = [];
-  for (let i = 0; i <= numberOfElements; i++) {
-    if (i < numberOfElements) array.push(i);
-    else {
-      array.push(i);
-      for (let k = numberOfElements - 1; k >= 0; k--) {
-        array.push(k);
-      }
-    }
-  }
-
-  console.log(array);
-};
-
-createArray(5);
-
-// Zoo inventory function
-
-let myZoo = [
-  ["King Kong", ["gorilla", 42]],
-  ["Nemo", ["fish", 5]],
-  ["Punxsutawney Phil", ["groundhog", 11]]
-];
-
-let zooInventory = inventory => {
-  let animals = [];
-  inventory.forEach(animal => {
-    animals.push(`${animal[0]} the ${animal[1][0]} is ${animal[1][1]}`);
-  });
-  console.log(animals);
-};
-
-zooInventory(myZoo);
-
-// INPUT: zooInventory(myZoo);
-// OUTPUT: [
-//   "King Kong the gorilla is 42",
-//   "Nemo the fish is 5",
-//   "Punzsutawney Phil the groundhog is 11"
-// ];
